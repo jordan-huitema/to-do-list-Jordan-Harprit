8af046e5f87fb2112daf48e594e5352abb88cbfd
Harprit singh <55721002+Harprit-singh@users.noreply.github.com>
2020-06-02 11:18:10 +1200

    Setup list add function

--- index.js
+++ index.js
@@ -5,6 +5,38 @@
 
 class Node {
     constructor(entry){
-
+    this.data = entry;
+    this.next = null;
     }
 }
+
+class Todolist{
+    constructor() {
+        this.head = null;
+        this.tail = null; 
+        this.length = 0;
+
+    } 
+    add (data){
+        var newNode = new Node(data)
+        if(!this.head) {
+            this.head = newNode
+            this.tail = newNode
+        }else {
+            this.tail.next = newNode
+            this.tail = newNode
+        }
+        this.length++
+        return this;
+    }
+}
+
+
+
+let list = new Todolist();
+
+console.log(list);
+list.add("nmit")
+list.add("cow")
+list.add("calf")
+console.log(list);
